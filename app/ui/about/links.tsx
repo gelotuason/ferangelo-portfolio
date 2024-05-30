a2e1e5d6413fba2cda7bafe3fe6648b5b2041d1d
gelotuason <gelotuason@gmail.com>
2024-05-30 13:04:32 +0800

    added target

--- app/ui/about/links.tsx
+++ app/ui/about/links.tsx
@@ -10,6 +10,7 @@ export function Links({ href, children }: LinkProps) {
         <Link
             href={href}
             className="px-[16px] py-[8px] rounded-xl bg-blue-700 text-white shadow-lg hover:shadow-blue-700/50"
+            target="blank"
         >
             {children}
         </Link>
