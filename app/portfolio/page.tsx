634aa9543853e642abfc9a8b39b5bdd8af8d320a
gelotuason <gelotuason@gmail.com>
2024-06-11 22:15:39 +0800

    added simple loading

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -7,6 +7,7 @@ import { getProjectsData } from "../utils/firebase";
 export default function Portfolio() {
 
     const [projectsData, setProjectsData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -19,33 +20,43 @@ export default function Portfolio() {
 
                     setProjectsData(parsedResult);
                 }
+
+                setLoading(false);
             } catch (error) {
+                setLoading(true);
+                alert(`Something didn't work properly, please refresh the page.`);
                 console.log(error);
             }
         }
         fetchData();
     }, []);
 
-    return (
-        <main className="max-w-[1280px] p-[32px] grid gap-10 mx-auto">
-            <div className="flex flex-col gap-0">
-                <p className="text-center text-gray-500 lg:text-lg">PROJECTS</p>
-                <h1 className="text-center font-bold text-3xl md:text-4xl lg:text-5xl">My Recent Projects</h1>
-            </div>
-            {
-                projectsData.map((project: any, index: number) => (
-                    <RecentProjects
-                        key={index}
-                        title={project.title}
-                        description={project.description}
-                        source_code_url={project.source_code_url}
-                        live_url={project.live_url}
-                        image_url={project.image_url}
-                        image_alt={project.image_alt}
-                        tech_stack={project.tech_stack}
-                    />
-                ))
-            }
-        </main>
-    )
+    if (loading) {
+        return (
+            <h1 className="h-5/6 p-[32px]">Loading...</h1>
+        )
+    } else {
+        return (
+            <main className="max-w-[1280px] p-[32px] grid gap-10 mx-auto">
+                <div className="flex flex-col gap-0">
+                    <p className="text-center text-gray-500 lg:text-lg">PROJECTS</p>
+                    <h1 className="text-center font-bold text-3xl md:text-4xl lg:text-5xl">My Recent Projects</h1>
+                </div>
+                {
+                    projectsData.map((project: any, index: number) => (
+                        <RecentProjects
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            source_code_url={project.source_code_url}
+                            live_url={project.live_url}
+                            image_url={project.image_url}
+                            image_alt={project.image_alt}
+                            tech_stack={project.tech_stack}
+                        />
+                    ))
+                }
+            </main>
+        )
+    }
 }
